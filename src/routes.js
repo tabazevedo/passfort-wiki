d5f77984e5b2a21cd39a543d4322b9b2b16e9a36
Tiago Azevedo <tabazevedo@gmail.com>
2018-04-12 11:44:39 +0100

    Tied documents list fetching to documents page

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,9 @@
+import { fetch as fetchDocuments } from './redux/actions/documents';
+
 export default {
   'route:documents': {
-    path: '/(documents)?'
+    path: '/(documents)?',
+    thunk: fetchDocuments()
   },
   'route:document': {
     path: '/documents/:name'
